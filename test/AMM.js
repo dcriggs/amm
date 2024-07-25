405e193c31781cc37e652208f2624523f4f78488
dcriggs <dawsoncriggs@gmail.com>
2024-07-25 11:08:26 -0600

    AMM contract deployment

---
+++ test/AMM.js
@@ -0,0 +1,71 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+const ether = tokens
+const shares = ether
+
+describe('AMM', () => {
+  let accounts,
+      deployer,
+      liquidityProvider,
+      investor1,
+      investor2
+
+  let token1,
+      token2,
+      amm
+
+  beforeEach(async () => {
+    // Setup Accounts
+    accounts = await ethers.getSigners()
+    deployer = accounts[0]
+    liquidityProvider = accounts[1]
+    investor1 = accounts[2]
+    investor2 = accounts[3]
+
+    // Deploy Token
+    const Token = await ethers.getContractFactory('Token')
+    token1 = await Token.deploy('Dapp University', 'DAPP', '1000000') // 1 Million Tokens
+    token2 = await Token.deploy('USD Token', 'USD', '1000000') // 1 Million Tokens
+
+    // Send tokens to liquidity provider
+    let transaction = await token1.connect(deployer).transfer(liquidityProvider.address, tokens(100000))
+    await transaction.wait()
+
+    transaction = await token2.connect(deployer).transfer(liquidityProvider.address, tokens(100000))
+    await transaction.wait()
+
+    // Send token1 to investor1
+    transaction = await token1.connect(deployer).transfer(investor1.address, tokens(100000))
+    await transaction.wait()
+
+    // Send token2 to investor2
+    transaction = await token2.connect(deployer).transfer(investor2.address, tokens(100000))
+    await transaction.wait()
+
+    // Deploy AMM
+    const AMM = await ethers.getContractFactory('AMM')
+    amm = await AMM.deploy(token1.address, token2.address)
+  })
+
+  describe('Deployment', () => {
+
+    it('has an address', async () => {
+      expect(amm.address).to.not.equal(0x0)
+    })
+
+    it('tracks token1 address', async () => {
+      expect(await amm.token1()).to.equal(token1.address)
+    })
+
+    it('tracks token2 address', async () => {
+      expect(await amm.token2()).to.equal(token2.address)
+    })
+
+  })
+
+})
