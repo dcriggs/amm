1cbb34d2aa90483933a0c576aadc73b3bc7bdf88
dcriggs <dawsoncriggs@gmail.com>
2024-07-27 09:44:13 -0600

    Add routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux';
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 //import { ethers } from 'ethers'
 
 // Components
 import Navigation from './Navigation';
+import Tabs from './Tabs';
+import Swap from './Swap';
+import Deposit from './Deposit';
+import Withdraw from './Withdraw';
+import Charts from './Charts';
 
 import {
   loadProvider,
@@ -19,41 +25,49 @@ function App() {
   const dispatch = useDispatch()
 
   const loadBlockchainData = async () => {
-      // Initiate provider
-      const provider = await loadProvider(dispatch)
+    // Initiate provider
+    const provider = await loadProvider(dispatch)
 
-      // Fetch current network's chainId (e.g. hardhat: 31337, kovan: 42)
-      const chainId = await loadNetwork(provider, dispatch)
+    // Fetch current network's chainId (e.g. hardhat: 31337, kovan: 42)
+    const chainId = await loadNetwork(provider, dispatch)
 
-      // Reload page when network changes
-      window.ethereum.on('chainChanged', () => {
-          window.location.reload()
-      })
+    // Reload page when network changes
+    window.ethereum.on('chainChanged', () => {
+      window.location.reload()
+    })
 
-      // Fetch current account from Metamask when changed
-      window.ethereum.on('accountsChanged', async () => {
-          await loadAccount(dispatch)
-      })
+    // Fetch current account from Metamask when changed
+    window.ethereum.on('accountsChanged', async () => {
+      await loadAccount(dispatch)
+    })
 
-      // Initiate contracts
-      await loadTokens(provider, chainId, dispatch)
-      await loadAMM(provider, chainId, dispatch)
+    // Initiate contracts
+    await loadTokens(provider, chainId, dispatch)
+    await loadAMM(provider, chainId, dispatch)
   }
 
   useEffect(() => {
-      loadBlockchainData()
+    loadBlockchainData()
   }, []);
 
   return (
     <Container>
-      <Navigation />
+      <HashRouter>
 
-      <h1 className='my-4 text-center'>React Hardhat Template</h1>
+        <Navigation />
 
-      <>
-        <p className='text-center'><strong>Your ETH Balance:</strong> {0} ETH</p>
-        <p className='text-center'>Edit App.js to add your code here.</p>
-      </>
+        <hr />
+
+        <Tabs />
+
+        <Routes>
+          <Route exact path="/" element={<Swap />} />
+          <Route path="/swap" element={<Swap />} />
+          <Route path="/deposit" element={<Deposit />} />
+          <Route path="/withdraw" element={<Withdraw />} />
+          <Route path="/charts" element={<Charts />} />
+        </Routes>
+      </HashRouter>
     </Container>
   )
 }
